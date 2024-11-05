ee5a8bc7fd1375255c7f259102ac2fa84d284d85
Juan Cruz <jcruzbaudino2406@gmail.com>
2024-11-05 16:56:57 -0300

    Cambios provisorios

--- Servidor/abb_sim_client.py
+++ Servidor/abb_sim_client.py
@@ -1,40 +1,39 @@
 import socket
 import time
-
 from interprete_gcode import SimuladorRobot
+
 class ABBSimClient:
     def __init__(self, host='localhost', port=8001):
         self.host = host
         self.port = port
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.host, self.port))
-        self.coordinates = []
 
     def send_coordinate(self, coordinate):
         """Envía una coordenada al servidor MATLAB."""
-        message = coordinate + "\n"
-        self.client_socket.sendall(message.encode('utf-8'))
-        print("Coordenada enviada:", coordinate)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((self.host, self.port))
+            message = coordinate + "\n"
+            client_socket.sendall(message.encode('utf-8'))
+            print("Coordenada enviada:", coordinate)
 
     def send_all_coordinates(self, coordinates_list):
         """Envía todas las coordenadas de la lista y envía el mensaje 'fin_gif' al final."""
-        for coord in coordinates_list:
-            self.send_coordinate(coord)
-        # Enviar mensaje para indicar el final de la transmisión
-        self.client_socket.sendall("fin_gif\n".encode('utf-8'))
-        print("Se enviaron todas las coordenadas. Finalizando guardado del GIF.")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((self.host, self.port))
+            for coord in coordinates_list:
+                message = coord + "\n"
+                client_socket.sendall(message.encode('utf-8'))
+                print("Coordenada enviada:", coord)
+                time.sleep(0.1)  # Pausa entre mensajes
+            # Enviar mensaje para indicar el final de la transmisión
+            client_socket.sendall("fin_gif\n".encode('utf-8'))
+            print("Se enviaron todas las coordenadas. Finalizando guardado del GIF.")
 
-    def close_connection(self):
-        """Envía el comando de salida y cierra la conexión."""
-        self.client_socket.sendall("salir\n".encode('utf-8'))
-        print("Comando de salida enviado. Finalizando cliente...")
-        self.client_socket.close()
-            
+
 if __name__ == "__main__":
     simuladorrobot = SimuladorRobot()
     client = ABBSimClient()
     movimientos = []
-    with open("instrucciones1.gcode", "r") as file:
+    with open("instrucciones5.gcode", "r") as file:
         contenido_gcode = file.read()
     simuladorrobot.procesar_gcode(contenido_gcode)
     print("Movimientos procesados:")
@@ -42,7 +41,6 @@ if __name__ == "__main__":
 
     print(client.coordinates)
     client.send_all_coordinates(client.coordinates)
-    client.close_connection()
 
 
 
